e4ddf2315d4146edaf0ae20e84d8f84a083252d4
Frangeul Nathan <seekerz17@outlook.fr>
2024-08-01 15:04:10 +0200

    first commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/articles', name: 'articles')]
+    public function articleFromDb(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('public/page/articles.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    #[Route('/article/{id}', name: 'show_article')]
+    public function showArticle(ArticleRepository $articleRepository, int $id): Response
+    {
+        $article = $articleRepository->find($id);
+
+        return $this->render('public/page/show_article.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+}
